c0168f46ec7f6440319c8327bf78670037cb495a
Chneemann <Andre-Ke@gmx.de>
2023-12-22 16:15:14 +0100

    update enemies size & spawnpoint

--- models/puffer-fish.class.js
+++ models/puffer-fish.class.js
@@ -3,5 +3,9 @@ class PufferFish extends MovableObject {
     super().loadImage(
       "./img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim1.png"
     );
+    this.x = 600;
+    this.y = 20 + Math.random() * 450;
+    this.height = 50;
+    this.width = 60;
   }
 }
