dad99104ea1acbf1f33acd1af5d8aea38cc4ed1a
Chneemann <Andre-Ke@gmx.de>
2023-12-23 13:18:21 +0100

    adjust size character

--- models/character.class.js
+++ models/character.class.js
@@ -1,4 +1,7 @@
 class Character extends MovableObject {
+  height = 200;
+  width = 250;
+
   constructor() {
     super().loadImage("./img/1.Sharkie/1.IDLE/1.png");
   }
--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -1,5 +1,5 @@
 class MovableObject {
-  x = 100;
+  x = 50;
   y = 200;
   img;
   height = 150;
