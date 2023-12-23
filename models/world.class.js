abebe6fbac0d02c59dacdb007ff3bd9bc923f674
Chneemann <Andre-Ke@gmx.de>
2023-12-23 13:15:12 +0100

    update background

--- models/world.class.js
+++ models/world.class.js
@@ -2,9 +2,11 @@ class World {
   character = new Character();
   enemies = [new PufferFish(), new PufferFish(), new PufferFish()];
   backgroundObjects = [
+    new BackgroundObject("img/3. Background/Layers/5. Water/D1.png", 0),
+    new BackgroundObject("img/3. Background/Layers/4.Fondo 2/D1.png", 0),
     new BackgroundObject("img/3. Background/Layers/3.Fondo 1/D1.png", 0),
     new BackgroundObject("img/3. Background/Layers/2. Floor/D1.png", 0),
-    new BackgroundObject("img/3. Background/Legacy/Layers/1. Light/2.png", 0),
+    new BackgroundObject("img/3. Background/Layers/1. Light/1.png", 0),
   ];
   canvas;
   ctx;
