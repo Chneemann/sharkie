ebdf1352a83e00057914d988bd5dbe5d143890c6
Chneemann <Andre-Ke@gmx.de>
2023-12-24 09:07:30 +0100

    add moveLeft to enemies

--- models/puffer-fish.class.js
+++ models/puffer-fish.class.js
@@ -7,5 +7,17 @@ class PufferFish extends MovableObject {
     this.y = 20 + Math.random() * 400;
     this.height = 50;
     this.width = 60;
+    this.moveLeft();
+  }
+
+  moveLeft() {
+    const speed = 1;
+    const interval = 50;
+
+    const move = () => {
+      this.x -= speed;
+    };
+
+    const movementInterval = setInterval(move, interval);
   }
 }
