f424b31de2a577d73f8ffb6fd5d70af8436006c5
Chneemann <Andre-Ke@gmx.de>
2023-12-25 19:38:00 +0100

    added swim animation for enemies

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -5,6 +5,7 @@ class MovableObject {
   height = 150;
   width = 200;
   imageCache = {};
+  currentImage = 0;
 
   loadImage(path) {
     this.img = new Image();
--- models/puffer-fish.class.js
+++ models/puffer-fish.class.js
@@ -1,16 +1,32 @@
 class PufferFish extends MovableObject {
+  IMAGES_SWIM = [
+    "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png",
+    "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim2.png",
+    "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim3.png",
+    "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim4.png",
+    "img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim5.png",
+  ];
+
   constructor() {
-    const images = [
-      "./img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png",
-      "./img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim1.png",
-      "./img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim1.png",
-    ];
-    super().loadImage(images[Math.floor(Math.random() * images.length)]);
+    super().loadImage(
+      "./img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png"
+    );
     this.x = 600;
     this.y = 20 + Math.random() * 400;
     this.height = 50;
     this.width = 60;
     this.animateEnemiesToLeft();
+    this.loadImages(this.IMAGES_SWIM);
+    this.animate();
+  }
+
+  animate() {
+    setInterval(() => {
+      let i = this.currentImage % this.IMAGES_SWIM.length;
+      let path = this.IMAGES_SWIM[i];
+      this.img = this.imageCache[path];
+      this.currentImage++;
+    }, 150);
   }
 
   animateEnemiesToLeft() {
