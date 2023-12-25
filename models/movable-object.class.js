cf5e001483128effd12a67c117f49a8ec14e5c34
Chneemann <Andre-Ke@gmx.de>
2023-12-25 19:28:40 +0100

    add character idle animation

--- models/movable-object.class.js
+++ models/movable-object.class.js
@@ -4,9 +4,18 @@ class MovableObject {
   img;
   height = 150;
   width = 200;
+  imageCache = {};
 
   loadImage(path) {
     this.img = new Image();
     this.img.src = path;
+  }
+
+  loadImages(arr) {
+    arr.forEach((path) => {
+      let img = new Image();
+      img.src = path;
+      this.imageCache[path] = img;
+    });
   }
 }
