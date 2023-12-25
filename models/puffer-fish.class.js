5ccbe9770865399958ac0efbe61d2198efa72526
Chneemann <Andre-Ke@gmx.de>
2023-12-25 11:58:59 +0100

    add random enemies images

--- models/puffer-fish.class.js
+++ models/puffer-fish.class.js
@@ -1,8 +1,11 @@
 class PufferFish extends MovableObject {
   constructor() {
-    super().loadImage(
-      "./img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim1.png"
-    );
+    const images = [
+      "./img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png",
+      "./img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim1.png",
+      "./img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim1.png",
+    ];
+    super().loadImage(images[Math.floor(Math.random() * images.length)]);
     this.x = 600;
     this.y = 20 + Math.random() * 400;
     this.height = 50;
@@ -13,6 +16,6 @@ class PufferFish extends MovableObject {
   animateEnemiesToLeft() {
     setInterval(() => {
       this.x -= 1;
-    }, 30);
+    }, 20);
   }
 }
