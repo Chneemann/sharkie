63853c67b17a6b4a3b56e595f9b8909ebe1ef502
Chneemann <Andre-Ke@gmx.de>
2023-12-26 11:31:52 +0100

    addEventListener Keyboard

--- js/game.js
+++ js/game.js
@@ -8,9 +8,37 @@ function init() {
 }
 
 window.addEventListener("keydown", (e) => {
-  if (e.keyCode == 38) {
+  if (e.code === "ArrowUp") {
     keyboard.UP = true;
-  } else {
+  }
+  if (e.code === "ArrowDown") {
+    keyboard.DOWN = true;
+  }
+  if (e.code === "ArrowRight") {
+    keyboard.RIGHT = true;
+  }
+  if (e.code === "ArrowLeft") {
+    keyboard.LEFT = true;
+  }
+  if (e.code === "Space") {
+    keyboard.SPACE = true;
+  }
+});
+
+window.addEventListener("keyup", (e) => {
+  if (e.code === "ArrowUp") {
     keyboard.UP = false;
+  }
+  if (e.code === "ArrowDown") {
+    keyboard.DOWN = false;
+  }
+  if (e.code === "ArrowRight") {
+    keyboard.RIGHT = false;
+  }
+  if (e.code === "ArrowLeft") {
+    keyboard.LEFT = false;
+  }
+  if (e.code === "Space") {
+    keyboard.SPACE = false;
   }
 });
