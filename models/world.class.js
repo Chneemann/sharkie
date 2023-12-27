a33e7fc505d6d905a8398009d51ccd2aa67b3512
Chneemann <Andre-Ke@gmx.de>
2023-12-27 11:46:37 +0100

    add levels

--- models/world.class.js
+++ models/world.class.js
@@ -1,29 +1,7 @@
 class World {
   character = new Character();
-  enemies = [new PufferFish(), new PufferFish(), new PufferFish()];
-  backgroundObjects = [
-    new BackgroundObject("img/3. Background/Layers/5. Water/D1.png", 0),
-    new BackgroundObject("img/3. Background/Layers/5. Water/D2.png", 719),
-    new BackgroundObject("img/3. Background/Layers/5. Water/D1.png", 719 * 2),
-    new BackgroundObject("img/3. Background/Layers/5. Water/D2.png", 719 * 3),
-    new BackgroundObject("img/3. Background/Layers/5. Water/D1.png", 719 * 4),
-    new BackgroundObject("img/3. Background/Layers/4.Fondo 2/D1.png", 0),
-    new BackgroundObject("img/3. Background/Layers/4.Fondo 2/D2.png", 719),
-    new BackgroundObject("img/3. Background/Layers/4.Fondo 2/D1.png", 719 * 2),
-    new BackgroundObject("img/3. Background/Layers/4.Fondo 2/D2.png", 719 * 3),
-    new BackgroundObject("img/3. Background/Layers/3.Fondo 1/D1.png", 0),
-    new BackgroundObject("img/3. Background/Layers/3.Fondo 1/D2.png", 719),
-    new BackgroundObject("img/3. Background/Layers/3.Fondo 1/D1.png", 719 * 2),
-    new BackgroundObject("img/3. Background/Layers/3.Fondo 1/D2.png", 719 * 3),
-    new BackgroundObject("img/3. Background/Layers/2. Floor/D1.png", 0),
-    new BackgroundObject("img/3. Background/Layers/2. Floor/D2.png", 719),
-    new BackgroundObject("img/3. Background/Layers/2. Floor/D1.png", 719 * 2),
-    new BackgroundObject("img/3. Background/Layers/2. Floor/D2.png", 719 * 3),
-    new BackgroundObject("img/3. Background/Layers/1. Light/1.png", 0),
-    new BackgroundObject("img/3. Background/Layers/1. Light/2.png", 719),
-    new BackgroundObject("img/3. Background/Layers/1. Light/1.png", 719 * 2),
-    new BackgroundObject("img/3. Background/Layers/1. Light/2.png", 719 * 3),
-  ];
+  enemies = level1.enemies;
+  backgroundObjects = level1.backgroundObjects;
   ctx;
   canvas;
   keyboard;
