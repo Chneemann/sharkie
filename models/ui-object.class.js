eb247cd07cf15ebcaae12e9b51df04dc2415cf71
Chneemann <Andre-Ke@gmx.de>
2023-12-31 18:12:25 +0100

    update ui elements

--- models/ui-object.class.js
+++ models/ui-object.class.js
@@ -4,6 +4,24 @@ class UiObject extends MovableObject {
   height = 50;
   width = 220;
   constructor() {
-    super().loadImage("./img/4. Marcadores/orange/100_  copia.png");
+    super().life();
+    this.coin();
+    this.poison();
+  }
+
+  life() {
+    this.x = 50;
+    this.y = 0;
+    this.loadImage("./img/4. Marcadores/orange/100_  copia.png");
+  }
+  coin() {
+    this.x = 50;
+    this.y = 50;
+    this.loadImage("./img/4. Marcadores/orange/0_  copia 2.png");
+  }
+  poison() {
+    this.x = 50;
+    this.y = 100;
+    this.loadImage("./img/4. Marcadores/orange/0_ copia.png");
   }
 }
