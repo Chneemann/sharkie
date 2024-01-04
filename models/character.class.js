e9c8144e8995858861297e39a4c7528376358c20
Chneemann <Andre-Ke@gmx.de>
2024-01-04 21:32:29 +0100

    add character attack images

--- models/character.class.js
+++ models/character.class.js
@@ -29,6 +29,26 @@ class Character extends MovableObject {
     "img/1.Sharkie/3.Swim/5.png",
     "img/1.Sharkie/3.Swim/6.png",
   ];
+  IMAGES_ATACK_MEELE = [
+    "img/1.Sharkie/4.Attack/Fin slap/1.png",
+    "img/1.Sharkie/4.Attack/Fin slap/2.png",
+    "img/1.Sharkie/4.Attack/Fin slap/3.png",
+    "img/1.Sharkie/4.Attack/Fin slap/4.png",
+    "img/1.Sharkie/4.Attack/Fin slap/5.png",
+    "img/1.Sharkie/4.Attack/Fin slap/6.png",
+    "img/1.Sharkie/4.Attack/Fin slap/7.png",
+    "img/1.Sharkie/4.Attack/Fin slap/8.png",
+  ];
+  IMAGES_ATACK_RANGE = [
+    "img/1.Sharkie/4.Attack/Bubble trap/For Whale/1.png",
+    "img/1.Sharkie/4.Attack/Bubble trap/For Whale/2.png",
+    "img/1.Sharkie/4.Attack/Bubble trap/For Whale/3.png",
+    "img/1.Sharkie/4.Attack/Bubble trap/For Whale/4.png",
+    "img/1.Sharkie/4.Attack/Bubble trap/For Whale/5.png",
+    "img/1.Sharkie/4.Attack/Bubble trap/For Whale/6.png",
+    "img/1.Sharkie/4.Attack/Bubble trap/For Whale/7.png",
+    "img/1.Sharkie/4.Attack/Bubble trap/For Whale/8.png",
+  ];
   IMAGES_HURT_MEELE = [
     "img/1.Sharkie/5.Hurt/1.Poisoned/1.png",
     "img/1.Sharkie/5.Hurt/1.Poisoned/2.png",
