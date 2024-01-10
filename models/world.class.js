c562455f155f93deff5758de4938d69be9c10f83
Chneemann <Andre-Ke@gmx.de>
2024-01-10 20:57:56 +0100

    bugfixes

--- models/world.class.js
+++ models/world.class.js
@@ -30,7 +30,7 @@ class World {
       this.level.enemies.forEach((enemy) => {
         if (this.character.isColliding(enemy)) {
           this.character.hit();
-          this.statusBar.setPercentage(this.character.hp);
+          this.statusBarHp.setPercentage(this.character.hp);
         }
       });
     }, 200);
