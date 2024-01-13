418091a7b5e19d47be43b965fb15f3377fb9ab0a
Chneemann <Andre-Ke@gmx.de>
2024-01-13 17:52:47 +0100

    update startscreen

--- js/game.js
+++ js/game.js
@@ -7,6 +7,10 @@ function init() {
   world = new World(canvas, keyboard);
 }
 
+function initLevel() {
+  document.getElementById("startscreen").classList.add("d-none");
+}
+
 window.addEventListener("keydown", (e) => {
   if (e.code === "ArrowUp") {
     keyboard.UP = true;
