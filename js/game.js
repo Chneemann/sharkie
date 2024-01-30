a4c915200370ac29837d707d1fe9efcc766f8069
Chneemann <Andre-Ke@gmx.de>
2024-01-30 11:22:28 +0100

    added fullscreen img

--- js/game.js
+++ js/game.js
@@ -39,6 +39,26 @@ function gameEndLost() {
   }, 1500);
 }
 
+function enterFullscreen(element) {
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.msRequestFullscreen) {
+    // for IE11 (remove June 15, 2022)
+    element.msRequestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    // iOS Safari
+    element.webkitRequestFullscreen();
+  }
+}
+
+function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  }
+}
+
 window.addEventListener("keydown", (e) => {
   if (e.code === "ArrowUp") {
     keyboard.UP = true;
