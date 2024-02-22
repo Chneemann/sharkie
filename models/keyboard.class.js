58cad328a629b7b8529deba80b76f0170f38a393
Chneemann <Andre-Ke@gmx.de>
2024-02-22 15:24:32 +0100

    JSDoc

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -38,92 +38,6 @@ class Keyboard {
     });
   }
 
-  btnKeyDownArrowUp(e) {
-    if (e.code === "ArrowUp" || e.code === "KeyW") {
-      this.UP = true;
-      this.MOVE = true;
-      this.lastInput = "up";
-      this.lastInputY = "up";
-      this.lastInputDate = new Date().getTime();
-    }
-  }
-
-  btnKeyDownArrowDown(e) {
-    if (e.code === "ArrowDown" || e.code === "KeyS") {
-      this.DOWN = true;
-      this.MOVE = true;
-      this.lastInput = "down";
-      this.lastInputY = "down";
-      this.lastInputDate = new Date().getTime();
-    }
-  }
-
-  btnKeyDownArrowRight(e) {
-    if (e.code === "ArrowRight" || e.code === "KeyD") {
-      this.RIGHT = true;
-      this.MOVE = true;
-      this.lastInput = "right";
-      this.lastInputX = "right";
-      this.lastInputDate = new Date().getTime();
-    }
-  }
-
-  btnKeyDownArrowLeft(e) {
-    if (e.code === "ArrowLeft" || e.code === "KeyA") {
-      this.LEFT = true;
-      this.MOVE = true;
-      this.lastInput = "left";
-      this.lastInputX = "left";
-      this.lastInputDate = new Date().getTime();
-    }
-  }
-
-  btnKeyDownArrowSpace(e) {
-    if (e.code === "Space") {
-      this.SPACE = true;
-      this.lastInputDate = new Date().getTime();
-    }
-  }
-
-  btnKeyUpArrowUp(e) {
-    if (e.code === "ArrowUp" || e.code === "KeyW") {
-      this.UP = false;
-      this.MOVE = false;
-      this.lastInputDate = new Date().getTime();
-    }
-  }
-
-  btnKeyUpArrowDown(e) {
-    if (e.code === "ArrowDown" || e.code === "KeyS") {
-      this.DOWN = false;
-      this.MOVE = false;
-      this.lastInputDate = new Date().getTime();
-    }
-  }
-
-  btnKeyUpArrowRight(e) {
-    if (e.code === "ArrowRight" || e.code === "KeyD") {
-      this.RIGHT = false;
-      this.MOVE = false;
-      this.lastInputDate = new Date().getTime();
-    }
-  }
-
-  btnKeyUpArrowLeft(e) {
-    if (e.code === "ArrowLeft" || e.code === "KeyA") {
-      this.LEFT = false;
-      this.MOVE = false;
-      this.lastInputDate = new Date().getTime();
-    }
-  }
-
-  btnKeyUpArrowSpace(e) {
-    if (e.code === "Space") {
-      this.SPACE = false;
-      this.lastInputDate = new Date().getTime();
-    }
-  }
-
   /**
    * Initializes event listeners for touchstart and touchend on game control buttons.
    * It sets the appropriate class properties based on the button pressed, allowing
@@ -224,5 +138,141 @@ class Keyboard {
    */
   btnPressed(btn) {
     return world.keyboard[btn];
+  }
+
+  /**
+   * Processes pressing the up arrow key or the W key.
+   *
+   * @param {KeyboardEvent} e - The KeyboardEvent.
+   */
+  btnKeyDownArrowUp(e) {
+    if (e.code === "ArrowUp" || e.code === "KeyW") {
+      this.UP = true;
+      this.MOVE = true;
+      this.lastInput = "up";
+      this.lastInputY = "up";
+      this.lastInputDate = new Date().getTime();
+    }
+  }
+
+  /**
+   * Processes pressing the down arrow button or the S button.
+   *
+   * @param {KeyboardEvent} e - The KeyboardEvent.
+   */
+  btnKeyDownArrowDown(e) {
+    if (e.code === "ArrowDown" || e.code === "KeyS") {
+      this.DOWN = true;
+      this.MOVE = true;
+      this.lastInput = "down";
+      this.lastInputY = "down";
+      this.lastInputDate = new Date().getTime();
+    }
+  }
+
+  /**
+   * Processes pressing the right arrow key or the D key.
+   *
+   * @param {KeyboardEvent} e - The KeyboardEvent.
+   */
+  btnKeyDownArrowRight(e) {
+    if (e.code === "ArrowRight" || e.code === "KeyD") {
+      this.RIGHT = true;
+      this.MOVE = true;
+      this.lastInput = "right";
+      this.lastInputX = "right";
+      this.lastInputDate = new Date().getTime();
+    }
+  }
+
+  /**
+   * Processes pressing the left arrow key or the A key.
+   *
+   * @param {KeyboardEvent} e - The KeyboardEvent.
+   */
+  btnKeyDownArrowLeft(e) {
+    if (e.code === "ArrowLeft" || e.code === "KeyA") {
+      this.LEFT = true;
+      this.MOVE = true;
+      this.lastInput = "left";
+      this.lastInputX = "left";
+      this.lastInputDate = new Date().getTime();
+    }
+  }
+
+  /**
+   * Processes the pressing of the space bar.
+   *
+   * @param {KeyboardEvent} e - The KeyboardEvent.
+   */
+  btnKeyDownArrowSpace(e) {
+    if (e.code === "Space") {
+      this.SPACE = true;
+      this.lastInputDate = new Date().getTime();
+    }
+  }
+
+  /**
+   * Processes the release of the up arrow key or the W key.
+   *
+   * @param {KeyboardEvent} e - The KeyboardEvent.
+   */
+  btnKeyUpArrowUp(e) {
+    if (e.code === "ArrowUp" || e.code === "KeyW") {
+      this.UP = false;
+      this.MOVE = false;
+      this.lastInputDate = new Date().getTime();
+    }
+  }
+
+  /**
+   * Processes the release of the up arrow button or the S button.
+   *
+   * @param {KeyboardEvent} e - The KeyboardEvent.
+   */
+  btnKeyUpArrowDown(e) {
+    if (e.code === "ArrowDown" || e.code === "KeyS") {
+      this.DOWN = false;
+      this.MOVE = false;
+      this.lastInputDate = new Date().getTime();
+    }
+  }
+
+  /**
+   * Processes the release of the up arrow key or the D key.
+   *
+   * @param {KeyboardEvent} e - The KeyboardEvent.
+   */
+  btnKeyUpArrowRight(e) {
+    if (e.code === "ArrowRight" || e.code === "KeyD") {
+      this.RIGHT = false;
+      this.MOVE = false;
+      this.lastInputDate = new Date().getTime();
+    }
+  }
+
+  /**
+   * Processes the release of the up arrow button or the A button.
+   *
+   * @param {KeyboardEvent} e - The KeyboardEvent.
+   */
+  btnKeyUpArrowLeft(e) {
+    if (e.code === "ArrowLeft" || e.code === "KeyA") {
+      this.LEFT = false;
+      this.MOVE = false;
+      this.lastInputDate = new Date().getTime();
+    }
+  }
+
+  /**
+   * Processes the release of the space bar.
+   *
+   * @param {KeyboardEvent} e - The KeyboardEvent.
+   */
+  btnKeyUpArrowSpace(e) {
+    if (e.code === "Space") {
+      this.SPACE = false;
+      this.lastInputDate = new Date().getTime();
+    }
   }
 }
